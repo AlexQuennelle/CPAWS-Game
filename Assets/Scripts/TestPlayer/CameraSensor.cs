b3e00befc1a260d708a1a2f968d8d0b0109d562a
Alex Quennelle <BluePhoenixCG@gmail.com>
2026-06-01 19:50:57 -0500

    Implement basic camera sensor scaffold

---
+++ Assets/Scripts/TestPlayer/CameraSensor.cs
@@ -0,0 +1,88 @@
+using UnityEngine;
+
+using System;
+using System.Collections.Generic;
+
+public class CameraSensor : MonoBehaviour
+{
+	[SerializeField]
+	private Camera _cam;
+
+	[SerializeField]
+	private float _nearPlane = 0.3f;
+	[SerializeField]
+	private float _farPlane = 50.0f;
+
+	[SerializeField]
+	private MeshCollider _frustumCol;
+	private Mesh _frustumMesh;
+
+	[SerializeField]
+	private RenderTexture _texture;
+
+	// [SerializeField]
+	private List<CameraTarget> _targets = new();
+
+	private void Start()
+	{
+		RecalculateSensor();
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		CameraTarget target = other.gameObject.GetComponent<CameraTarget>();
+		if (target != null)
+		{
+			_targets.Add(target);
+		}
+	}
+	void OnTriggerExit(Collider other)
+	{
+		CameraTarget target = other.gameObject.GetComponent<CameraTarget>();
+		if (target != null)
+		{
+			_targets.Remove(target);
+		}
+	}
+
+	public void TakePicture()
+	{
+		Debug.Log("Taking Picture");
+	}
+
+	private void RecalculateSensor()
+	{
+		if (_cam != null)
+		{
+			int resX = _cam.scaledPixelWidth;
+			int resY = _cam.scaledPixelHeight;
+			Vector3 offset = _cam.transform.parent.localPosition;
+			Vector3[] frustumVerts = {
+				_cam.ScreenToWorldPoint(new(0.0f, 0.0f, _nearPlane)) - offset,
+				_cam.ScreenToWorldPoint(new(0.0f, resY, _nearPlane)) - offset,
+				_cam.ScreenToWorldPoint(new(resX, resY, _nearPlane)) - offset,
+				_cam.ScreenToWorldPoint(new(resX, 0.0f, _nearPlane)) - offset,
+
+				_cam.ScreenToWorldPoint(new(0.0f, 0.0f, _farPlane))  - offset,
+				_cam.ScreenToWorldPoint(new(0.0f, resY, _farPlane))  - offset,
+				_cam.ScreenToWorldPoint(new(resX, resY, _farPlane))  - offset,
+				_cam.ScreenToWorldPoint(new(resX, 0.0f, _farPlane))  - offset,
+			};
+			int[] frustumTris = {
+				0, 1, 2,  0, 2, 3, // Front face
+				4, 5, 1,  4, 1, 0, // Left face
+				7, 6, 5,  4, 7, 5, // Back face
+				3, 2, 6,  3, 6, 7, // Right face
+				1, 5, 6,  1, 6, 2, // Top face
+				4, 0, 3,  4, 3, 7  // Bottom face
+			};
+			_frustumMesh = new Mesh
+			{
+				vertices = frustumVerts,
+				triangles = frustumTris,
+			};
+			_frustumMesh.RecalculateNormals();
+			_frustumCol.sharedMesh = _frustumMesh;
+		}
+	}
+}
---
+++ Assets/Scripts/TestPlayer/CameraTarget.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class CameraTarget : MonoBehaviour
+{
+	public int GetScore()
+	{
+		return 0;
+	}
+}
