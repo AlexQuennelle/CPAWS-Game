cb97695ef20675a59705d708f48fc8cc81c28820
Conor Daly <conordaly2525@gmail.com>
2026-06-03 11:16:10 -0500

    Added a moose for testing purposes

---
+++ Assets/AnimalPrototyping/Moose/MooseStateMachine.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class MooseStateMachine : AnimalStateMachine
+{
+	void Start()
+	{
+		Debug.Log("Moose State Machine Started");
+	}
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
